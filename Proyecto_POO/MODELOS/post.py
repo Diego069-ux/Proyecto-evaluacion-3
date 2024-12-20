8a072c1b371a6004f728102737329c61385283d5
Diego069-ux <diegobenjamin.in@gmail.com>
2024-12-20 17:15:56 -0300

    Update post.py

--- Proyecto_POO/MODELOS/post.py
+++ Proyecto_POO/MODELOS/post.py
@@ -1,9 +1,11 @@
-
+# Modelos/post.py
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
 class Post(db.Model):
+    __tablename__ = 'posts'  # Nombre de la tabla en la base de datos
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    body = db.Column(db.String(300), nullable=False)
+    body = db.Column(db.Text, nullable=False)
